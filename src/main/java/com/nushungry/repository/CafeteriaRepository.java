0a0bc3cbd114150ff47107fb69d422a3d8ab8d2e
1zero224 <754413199@qq.com>
2025-09-30 14:04:49 +0800

    fix: Add missing import statements to support query parameters

--- src/main/java/com/nushungry/repository/CafeteriaRepository.java
+++ src/main/java/com/nushungry/repository/CafeteriaRepository.java
@@ -1,5 +1,7 @@
 package com.nushungry.repository;
 
+import java.util.Optional;
+import org.springframework.data.repository.query.Param;
 import com.nushungry.model.Cafeteria;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
