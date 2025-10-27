b93aa83b767be919ab1d87c331bc77ccf1dca0c0
1zero224 <754413199@qq.com>
2025-10-27 23:23:28 +0800

    keep only the monolithic architecture part

--- admin-service/src/main/java/com/nushungry/adminservice/dto/ChangePasswordRequest.java
+++
@@ -1,8 +0,0 @@
-package com.nushungry.adminservice.dto;
-
-import lombok.Data;
-
-@Data
-public class ChangePasswordRequest {
-    private String password;
-}
--- media-service/src/main/java/com/nushungry/mediaservice/repository/MediaFileRepository.java
+++
@@ -1,7 +0,0 @@
-package com.nushungry.mediaservice.repository;
-
-import com.nushungry.mediaservice.model.MediaFile;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface MediaFileRepository extends JpaRepository<MediaFile, Long> {
-}
